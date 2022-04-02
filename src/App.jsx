e50861cc63634a1324ab7444c4b4fc02b674552a
Frankz <leninalarcon7@gmail.com>
2022-04-01 23:30:23 -0500

    redirect from / to /customers

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import {
+  BrowserRouter, Routes, Route, Navigate,
+} from 'react-router-dom';
 import Layout from './Layout/Layout';
 import NewClient from './pages/NewClient';
 import EditClient from './pages/EditClient';
@@ -10,6 +12,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/customers" />} />
         <Route path="/customers" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="new" element={<NewClient />} />
